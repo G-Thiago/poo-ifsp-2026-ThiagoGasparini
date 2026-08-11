f3bc6399ba086886e7c1981c036f571991b673f3
Thiago Gazolla Gasparini <thiagogazolla85@gmail.com>
2026-08-11 12:03:06 -0300

    Exercicio de triangulo

---
+++ HackerRank/src/aula03/triangulo.java
@@ -0,0 +1,50 @@
+package aula03;
+
+import java.util.Scanner;
+
+public class triangulo {
+
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner (System.in);
+
+        double medida[] = new double[3];
+        double perimetro;
+        double area;
+
+        for ( int i = 0; i < 3; i++){
+
+            System.out.printf("Entre com o lado %d", i);
+            medida[i] = sc.nextDouble();
+
+        }
+
+
+        boolean isTrue = medida[0] < medida[1] + medida[2] &&
+                         medida[1] < medida[2] + medida[0] &&
+                         medida[2] < medida[0] + medida[1];
+
+        if ( isTrue){
+
+            perimetro = medida[0] + medida[1] + medida[2];
+
+            System.out.println("Perimetro = " + perimetro);
+
+
+        }else{
+
+            area = ((medida[0] + medida[1]) * medida[2]) /2 ;
+
+            System.out.println("Area = " + area);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
