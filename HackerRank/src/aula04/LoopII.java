545a9958a449ce492d12af91dd9a27a04194788a
Thiago Gazolla Gasparini <thiagogazolla85@gmail.com>
2026-08-18 18:23:26 -0300

    5/5 exercícios passados

--- HackerRank/src/aula04/LoopII.java
+++ HackerRank/src/aula04/LoopII.java
@@ -6,8 +6,28 @@ public class LoopII {
 
     public static void main(String[] args){
 
+        Scanner sc = new Scanner(System.in);
 
+        int q = sc.nextInt();
 
+        for ( int i = 0; i < q;i++){
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+
+                int temp = a;
+                for ( int j = 0; j < n; j++){
+
+                    temp += b * (int)Math.pow(2, j);
+                    System.out.print(temp + " ");
+
+                }
+                System.out.println();
+
+        }
+
+            sc.close();
 
 
     }
